1724560f934fa8ed1502439d5574137209cf0c1c
Skylar <skylarblessings5@gmail.com>
2023-03-12 03:20:48 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+  function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+  }
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    console.log(My name is ${name} and I am learning ${language}.);
+    }
+    
+    
